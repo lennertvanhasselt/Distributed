a88052c9cfcf65b0c8d12b7a58fc437872f72c1c
Jonas7100 <jonas7100@hotmail.com>
2016-10-25 12:17:58 +0200

    node1

---
+++ src/node1/Node1.java
@@ -0,0 +1,16 @@
+package node1;
+
+import java.util.Scanner;
+
+public class Node1 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String Nodename;
+		
+		System.out.println("Give the name of the node: ");
+		Nodename=scan.nextLine();
+		scan.close();
+	}
+
+}

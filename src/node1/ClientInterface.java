209d14eb0ace300ce068a3d07ff1b6072f17a0ed
Jonas7100 <jonas7100@hotmail.com>
2016-10-25 12:38:28 +0200

    Node1.0

---
+++ src/node1/ClientInterface.java
@@ -0,0 +1,8 @@
+package node1;
+import java.net.InetAddress;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface ClientInterface extends Remote{
+	int setNode(String clientName, InetAddress IP)throws RemoteException;
+}
--- src/node1/Node1.java
+++ src/node1/Node1.java
@@ -1,16 +1,31 @@
 package node1;
 
 import java.util.Scanner;
+import java.net.InetAddress;
+import java.rmi.*;
+import node1.ClientInterface;
 
 public class Node1 {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		String Nodename;
+		String nodename;
+		InetAddress IP = null;
 		
-		System.out.println("Give the name of the node: ");
-		Nodename=scan.nextLine();
-		scan.close();
+		try {
+			String name = "Moet nog aangepast worden";
+			ClientInterface ci = (ClientInterface) Naming.lookup(name);
+			
+			System.out.println("Give the name of the node: ");
+			nodename=scan.nextLine();
+			scan.close();
+			IP = InetAddress.getLocalHost();	
+			ci.setNode(name, IP);
+			
+		} catch(Exception e) {
+	         System.err.println("FileServer exception: "+ e.getMessage());
+	         e.printStackTrace();
+	    }
 	}
 
 }

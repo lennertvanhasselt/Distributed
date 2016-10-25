65a5b88d733cc7f5ab4a3ea2afafa9891774fa8b
LiamOorts <liam.oorts@hotmail.com>
2016-10-25 11:57:52 +0200

    qdsfqsdf

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.lang.*;
 
 //GROUP 8 FTW zwam
 
@@ -6,9 +7,24 @@ public class Main {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		String file;
+		String filename;
 		
-		file = sc.nextLine();
+		System.out.println("Name of the node: ");
+		filename = sc.nextLine();
+		
+		int length = filename.length();
+		
+		char[] fileChars = filename.toCharArray();
+		
+		int sum=0;
+		for(int i=0;i<length;i++)
+			sum=sum*97+(i+1)*Character.getNumericValue(fileChars[i]);
+		
+		sum = Math.abs(sum)%32768;
+		
+		System.out.println(sum);
+		
+		sc.close();
 	}
 
 }

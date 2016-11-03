6942476bd81361de44e3a5fffd0ab99fe98dc28c
Jonas7100 <jonas7100@hotmail.com>
2016-11-03 14:23:06 +0100

    Node 1 change

--- src/Node1.java
+++ src/Node1.java
@@ -1,4 +1,3 @@
-
 import java.net.InetAddress;
 import java.rmi.Naming;
 import java.util.Scanner;
@@ -9,6 +8,7 @@ public class Node1 {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 	 	String Nodename;
+	 	int choice=0;
 		try {
 			String name = "//192.168.1.3/cliNode";
    	 		ClientInterface cf = (ClientInterface) Naming.lookup(name);
@@ -20,14 +20,29 @@ public class Node1 {
    	 		
    	 		int ownNode = cf.setNode(Nodename, address); 
    	 		
-   	 		//HIER WHILE LUS MET OPTIES TOEVOEGEN OM TE BESTUREN
-   	 		
+   	 		while(choice < 4)
+   	 		{
+   	 			choice = menu(scan);
+   	 		}
    	 		scan.close();
 	
 		} catch(Exception e) {
 	         System.err.println("FileServer exception: "+ e.getMessage());
 	       e.printStackTrace();
 	    }
+	}
+	
+	static int menu(Scanner scan)
+	{
+	 	System.out.println("                  Menu                  ");
+	 	System.out.println("----------------------------------------");
+	 	System.out.println("1) Bestand opvragen");
+	 	System.out.println("2) ");
+	 	System.out.println("3) ");
+	 	System.out.println("4) Afsluiten");
+	 	System.out.println("Geef u keuzen in: ");
+	 	int menuChoice = scan.nextInt();
+		return menuChoice;	
 	}
 
 }

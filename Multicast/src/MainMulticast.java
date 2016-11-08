fbe689aee35285de54a6e3eb4417147f381cfa55
lennertvanhasselt <lennert.vanhasselt@outlook.com>
2016-11-08 09:27:42 +0100

    Test MainMulticastServer

--- Multicast/src/MainMulticast.java
+++ Multicast/src/MainMulticast.java
@@ -1,9 +1,32 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+import java.net.UnknownHostException;
 
-public class MainMulticast {
+public class MainMulticast extends Thread {
+    
+    final static String INET_ADDR = "225.1.1.1";
+    final static int PORT = 8888;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		d
-	}
+    public static void main(String[] args) throws UnknownHostException, InterruptedException {
+        // Get the address that we are going to connect to.
+        InetAddress group = InetAddress.getByName(INET_ADDR);
+     
+        // Open a new DatagramSocket, which will be used to send the data.
+        try (MulticastSocket serverSocket = new MulticastSocket(PORT)) {
+        	serverSocket.joinGroup(group);
+                String msg = "192.168.1.12";
 
+                // Create a packet that will contain the data
+                // (in the form of bytes) and send it.
+                DatagramPacket msgPacket = new DatagramPacket(msg.getBytes(),
+                        msg.getBytes().length, group, PORT);
+                serverSocket.send(msgPacket);
+                System.out.println("Server sent multicast packet with message: " + msg);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
 }

e1363b3e63a7cd62ad138beaaf2123d1dcb4f30c
lennertvanhasselt <lennert.vanhasselt@outlook.com>
2016-11-08 11:06:48 +0100

    multicast sender in system Y

---
+++ src/MulticastSender.java
@@ -0,0 +1,32 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+
+public class MulticastSender implements Runnable {
+	
+	private InetAddress ip;
+	private String hostname;
+	
+	public MulticastSender(InetAddress ip, String hostname){
+		this.ip = ip;
+		this.hostname = hostname;
+	}
+
+	@Override
+	public void run() {
+		try{
+			MulticastSocket socket = new MulticastSocket(8888);
+			socket.joinGroup(ip);
+			
+	        DatagramPacket msgPacket = new DatagramPacket(hostname.getBytes(),hostname.getBytes().length, ip, 8888);
+	        socket.send(msgPacket);
+	        System.out.println("Sent multicast packet with message: " + hostname);
+			
+        } catch (IOException ex) {
+          ex.printStackTrace();
+        }	
+		
+	}
+
+}

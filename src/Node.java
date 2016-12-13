ef95cad5b7949c252a288eba9fb8b4a41b92db10
ThijsAnthonis <thijsanthonis@hotmail.com>
2016-12-13 12:48:04 +0100

    Hopelijke fix ivm niet deleten van repicated files (niet zeker)(waarschijnlijk niet)(tis het proberen waard)

--- src/Node.java
+++ src/Node.java
@@ -610,8 +610,10 @@ public class Node extends UnicastRemoteObject implements NodeInterface {
 		}
 		
 		for(int i = filesToRemove.size(); i>0; i--) {
-			deleteFile(replicatedFiles.get(filesToRemove.get(i)));
-			replicatedFiles.remove(filesToRemove.get(i));
+			System.out.print("Deleting replicated files... ");
+			File file = new File("C:/temp/replicated/"+replicatedFiles.get(filesToRemove.get(i)).getNameFile());
+			file.delete();
+			replicatedFiles.remove(replicatedFiles.get(filesToRemove.get(i)));
 		}
 	}
 

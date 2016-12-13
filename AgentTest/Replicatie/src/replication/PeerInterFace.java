e1e36dfe3080b07c7f59c31619eed58398122868
Jonas7100 <jonas7100@hotmail.com>
2016-12-13 14:20:43 +0100

    hopelijk nu wel gefixt ;) thijs

--- AgentTest/Replicatie/src/replication/PeerInterFace.java
+++
@@ -1,7 +0,0 @@
-package replication;
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface PeerInterFace extends Remote{
-	public int setupTCPReceiver(String fileName, int fileLength)throws RemoteException, ClassNotFoundException;
-}
